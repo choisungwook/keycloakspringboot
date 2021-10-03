52c71911ab6501ec61dae90c218b9ec6e993e44a
choisungwook <kgg1959@naver.com>
2021-10-03 16:06:30 +0900

    프론트엔드: 메인화면 구성 #4

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,16 @@
 <template>
-  <v-container>
-    <v-row>
-      <router-view></router-view>
+  <v-container class="pt-10">
+    <v-row align="center" justify="space-around">
+      <v-btn color="error">스프링부트 /anyone api요청</v-btn>
+    </v-row>
+    <v-row align="center" justify="space-around" class="pt-10">
+      <v-btn color="error">스프링부트 /usera api요청</v-btn>
+    </v-row>
+    <v-row align="center" justify="space-around" class="pt-10">
+      <v-btn color="error">스프링부트 /userb api요청</v-btn>
+    </v-row>
+    <v-row align="center" justify="space-around" class="pt-10">
+      <v-btn color="error">스프링부트 /admin api요청</v-btn>
     </v-row>
   </v-container>
 </template>
