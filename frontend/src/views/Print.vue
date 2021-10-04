c9c2bea95cce6ae1eb0b5bd276b5524cb7fe1c21
choisungwook <kgg1959@naver.com>
2021-10-04 17:59:25 +0900

    프론트엔드 라우터 추가

---
+++ frontend/src/views/Print.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container class="pt-10">
+    {{ auth }}
+    {{ token }}
+    {{ idToken }}
+  </v-container>
+</template>
+
+<script>
+import Vue from "vue";
+export default {
+  data: () => ({
+    auth: !Vue.$keycloak.authenticated,
+    token: !Vue.$keycloak.token,
+    idToken: !Vue.$keycloak.idToken,
+  }),
+};
+</script>
